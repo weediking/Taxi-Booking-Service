9b018c43479a0991afcc9af0bcd620dc95728b24
WaleedMehmood123 <waleedmehmood054@gmail.com>
2023-09-12 23:18:12 +0500

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter,Outfit } from 'next/font/google'
+import NavBar from '@/components/NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+      <body className={outfit.className}>
+        <NavBar />
+        {children}
+      </body>
+      </html>
+    </ClerkProvider>
   )
 }
